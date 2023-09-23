9d707a2e070ea3a76cad192a657e842f81dafca0
WatchingWatches <106354710+WatchingWatches@users.noreply.github.com>
2023-09-23 16:06:26 +0200

    Update Filament_approx.py

--- Filament_approx.py
+++ Filament_approx.py
@@ -2,15 +2,13 @@
 """
 Created on Thu May 26 23:36:36 2022
 
-@author: bjans
 reddit: watching-watches
-Formula to approximate the length and mass of the filament on your spool
-Please note: this is only a approximation this only works accurate if the spool is wound properly
+Formula to approximate the length of the filament on your spool
 """
 import numpy as np
 from colorama import Fore, Style
+from math import floor
 
-density = 1.27 #Pla 1.25; PETG 1.27 enter the value for density here
 """
 How to use:
 r1 := inner radius of spool
@@ -20,41 +18,62 @@ d_f := diameter of filament
 Type for example: 'lenght(50,71,45,1.75)' all units in mm in the command window
 and press enter. The result will be in meters and gram.
 """
-def length(r1,r2,b,d_f):
-    L=[]
-    k = 1 #start point of the sum
+def length(r1,r2,b,d_f,density):
+    L = 0
     n = (r2-r1)/d_f #number of layers
-    rows = (b/d_f) #number of rows
+    rows = (b/d_f)-1 #number of rows minus one for safety reasons
     #rounds the numbers to a whole number:
-    round(n)
-    round(rows)
+    n = floor(n) #rounds down
+    rows = floor(rows)
     A = np.pi*(d_f/2)**2 #area of filament cross section
-    while k <= n:
-        def f(x):
-             l = 2*np.pi*(r1+d_f*x-d_f/2)*rows
-             return l
-        L.append(f(k)) #writes lists with length of each layer
-        k+=1
-    V = 0.9*10**-3*sum(L) #sums the list and konverts it to meters
-    Vol = A*V #volium
-    m = Vol*density #mass
-    #this warns you when you may have entered wrong values
-    if (r1>=r2):
-        print(Fore.RED + 'r2 needs to be greater than r1:',V,'is not the lenght of your filament')
+    if(n >80): #with big spools you may need to change the 80 to a bigger number
+        print("Number of Layers:",n,"is too big!")#prevents too much calculation
         
-    if (r1>b):
-        print(Fore.RED + 'r1 is greater than b are you sure you entered the right parameters?')
+    else:
+        for k in range(1,int(n)+1,1):
+            l = 2*np.pi*(r1+d_f*k-d_f/2)*rows
+            L += l
+       
+        V = 0.9*10**-3*L #sums the list and converts it to meters
+        Vol = A*V #volume
+        m = Vol*density #mass
         
-    if (d_f>3):
-        print(Fore.RED + 'd_f is greater than 3 are you sure you entered the right parameters?')
+        warnings = True # enable or disable warnings
         
-    print(Style.RESET_ALL) #the values will not be red
-    print(V,'[m]')
-    print('density used:',density,'[g/cm^3]')
-    print(m,'[g]')
+        if(warnings == True):
+            #this warns you when you may have entered wrong values
+            if (r1>=r2):
+                print(Fore.RED + 'r2 needs to be greater than r1:',V,'is not the lenght of your filament!')
+                
+            if (r1>b):
+                print(Fore.RED + 'r1 = ',r1,' is greater than b = ',b,' are you sure you entered the right parameters?')
+                
+            if (d_f>3):
+                print(Fore.RED + 'd_f = ',d_f,' is greater than 3 are you sure you entered the right parameters?')
+                
+            if (r1<=0 or r2 <= 0 or b<= 0 or d_f<= 0 or density<= 0):
+                print(Fore.RED + 'Some values are smaller than 0 the calculation is wrong!') #(negative values)
+            
+        print(Style.RESET_ALL)    
+        print(V,'[m]') 
+        print(m,'[g]')
+        print('density used:',density,'[g/cm^3]')
+        print(n,'layers')
     
+"""
+Density:
+  PLA 1.25
+  PETG 1.24   
+"""
+
 #you can save the parameters of your spool in a function like this, so you only need to enter
-#the data of r2 when using it again
-def Giantarm(r2):#1kg
-    length(30,r2,53,1.75)
+#the data of r2
+def Giantarm(r2,density):#1kg
+    length(40,r2,53,1.75,density)
+    
+def Prima3d(r2,density):#750g
+    length(52,r2,45,1.75,density)
+    
+def eSun(r2,density):
+    length(95/2,r2,57,1.75,density)
     
